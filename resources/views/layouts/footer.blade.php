71b64144957692a62f81fd7ad9d9dad861f09401
Petr Nikitin <petr.niki.777@gmail.com>
2024-10-18 16:23:10 +0300

    созданы шаблоны и главная страница

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,68 @@
+</main>
+</div>
+<footer class="footer page__footer">
+    <div class="footer__container">
+        <div class="footer__wrapper">
+            <div class="footer__nav">
+                <div class="footer__menu">
+                    <h2 class="footer__title">Меню</h2>
+                    <ul class="footer__list clear-list">
+                        <li class="footer__item"><a href="./services.html" class="footer__link">Услуги</a></li>
+                        <li class="footer__item"><a href="./carpark.html" class="footer__link">Автопарк</a></li>
+                        <li class="footer__item"><a href="./about.html" class="footer__link">О нас</a></li>
+                        <li class="footer__item"><a href="./projects.html" class="footer__link">Проекты</a></li>
+                        <li class="footer__item"><a href="./contacts.html" class="footer__link">Контакты</a></li>
+                    </ul>
+                </div>
+                <div class="footer__menu">
+                    <h2 class="footer__title">Услуги</h2>
+                    <ul class="footer__list clear-list">
+                        <li class="footer__item"><a href="" class="footer__link">Перевозка негабаритных
+                                грузов</a></li>
+                        <li class="footer__item"><a href="" class="footer__link">Перевозка
+                                экскаваторов</a></li>
+                        <li class="footer__item"><a href="" class="footer__link">Перевозка негабаритных
+                                грузов</a></li>
+                        <li class="footer__item"><a href="" class="footer__link">Перевозка сборных грузов
+                                грузов</a></li>
+                        <li class="footer__item"><a href="" class="footer__link">Перевозка негабаритных
+                                грузов</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer__contacts">
+                <div class="footer__col"><a href="tel:+79778731886" class="footer__link footer__link--phone">+7
+                        (977) 873-18-86</a> <a href="tel:+79778731886" class="footer__link footer__link--phone">+7
+                        (977) 873-18-86</a> <button type="button" class="btn footer__btn btn--dark">Заказать
+                        звонок</button>
+                    <div class="footer__mail"><a class="footer__link" href="mailto:infoinfo@gmail.com"><svg
+                                class="footer__mail-svg">
+                                <use xlink:href="#mail--inline"></use>
+                            </svg> </a><span class="footer__mail-span">infoinfo@gmail.com</span></div>
+                </div>
+                <div class="footer__col">
+                    <div class="footer__office">
+                        <p class="footer__text">Главный офис</p>
+                        <p class="footer__text">Россия, Московская обл., г. Королев, ул. Пионерская, д.14А</p>
+                    </div>
+                    <div class="footer__social">
+                        <p class="footer__text">Наши соцсети</p>
+                        <div class="social footer__icons"><a href="#" class="social__link"><svg
+                                    class="social__svg">
+                                    <use xlink:href="#tg--inline"></use>
+                                </svg> </a><a href="#" class="social__link"><svg class="social__svg">
+                                    <use xlink:href="#wa--inline"></use>
+                                </svg></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="footer__rights">2024. Все права защищены</p><a class="footer__back-btn"></a>
+    </div>
+</footer>
+<script src="{{ asset('./js/jquery.min.js') }}"></script>
+<script src="{{ asset('./js/slick.min.js') }}"></script>
+<script src="{{ asset('./js/index.min.js') }}" type="module"></script>
+</body>
+
+</html>
